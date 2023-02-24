f8eb59699c67dc38caf86bd41ce573ad3b799778
Benatia <mouatamidbenatia24@gmail.com>
2023-02-24 16:35:33 +0100

    (chore): Rebased main on master

--- src/main/java/fr/epsi/netflix/MovieGenerator.java
+++ src/main/java/fr/epsi/netflix/MovieGenerator.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class MovieGenerator {
 
-    public static void generateNeNIO(String file){
+    public static void generateNeNIO(String file) {
         try {
             List<String> datas = Files.readAllLines(Paths.get(file))
                     .stream()
@@ -18,12 +18,12 @@ public class MovieGenerator {
                     .collect(Collectors.toList());
 
             List<Movie> movies = MovieFactory.getInstance().createMovies(datas);
-            
+
             HtmlFactory htmlFactory = new HtmlFactory();
             String html = htmlFactory.generateHtml(movies);
 
             Path folderOut = Path.of("out");
-            if(!Files.exists(folderOut)){
+            if (!Files.exists(folderOut)) {
                 Files.createDirectory(folderOut);
             }
 
@@ -33,8 +33,8 @@ public class MovieGenerator {
             Path folder = Paths.get("out/movies");
             folder.toFile().mkdirs();
 
-            for(Movie movie:movies){
-                Path fileMovie = Paths.get("out/movies", movie.getId()+".html");
+            for (Movie movie : movies) {
+                Path fileMovie = Paths.get("out/movies", movie.getId() + ".html");
                 Files.write(fileMovie, htmlFactory.generateDetail(movie).getBytes());
             }
 
@@ -44,7 +44,7 @@ public class MovieGenerator {
     }
 
     public static void main(String[] args) {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.err.println("Utilisation : java -jar <lib> <fichier_csv>");
             System.exit(-1);
             return;
